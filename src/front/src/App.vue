b9d9c6ac9c5793eade3301128fdd9fde45452dbb
vitorcdb111 <vitordebortoli@hotmail.com>
2021-09-22 10:03:40 -0300

    arrumando painel superior direito

--- src/front/src/App.vue
+++ src/front/src/App.vue
@@ -110,32 +110,20 @@
         <!-- End of Left-panel -->
         <!-- Right-panel -->
         <div id="right-panel">
-          <div class="left-top-container">
-            <svg height="100" width="225" class="left-svg">
+          <!-- Blank container -->
+          <div class="empty-container">
+            <svg height="100%" width="100%">
               <polyline
-                  points="0,75 70,75 90,38 224,38"
+                  points="0,0 0,40 138,40 158,75 250,75 250,0 0,0"
+                  style="fill: #f2f2f2; stroke: none; stroke-width: 3"
+              />
+              <polyline
+                  points="0,40 138,40 158,75 250,75"
                   style="fill: none; stroke: black; stroke-width: 3"
               />
             </svg>
-            <div class="lights-container">
-              <div class="big-light-boarder">
-                <div class="big-light blue">
-                  <div class="big-dot light-blue"></div>
-                </div>
-              </div>
-              <div class="small-lights-container">
-                <div class="small-light red">
-                  <div class="dot light-red"></div>
-                </div>
-                <div class="small-light yellow">
-                  <div class="dot light-yellow"></div>
-                </div>
-                <div class="small-light green">
-                  <div class="dot light-green"></div>
-                </div>
-              </div>
-            </div>
           </div>
+          <!-- Top screen -->
           <div class="top-screen-container">
             <div v-if="tipos.length" id="about-screen" class="right-panel-screen">
               Weight: {{retorno.weight}}kg
